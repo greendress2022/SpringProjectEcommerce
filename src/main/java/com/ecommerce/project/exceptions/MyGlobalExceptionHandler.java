c02b6bd55f188af42ba38342192367e8f01f4948
yuankuang <kuang.yua@northeastern.edu>
2024-12-01 15:41:21 -0800

    handle different exceptions in a place

---
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.ecommerce.project.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String,String> errors = new HashMap<>();
+        for(FieldError fieldError: e.getBindingResult().getFieldErrors()){
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(APIExceptions.class)
+    public ResponseEntity<String> myAPIException(APIExceptions e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST); //called on a bad api request
+    }
+}
