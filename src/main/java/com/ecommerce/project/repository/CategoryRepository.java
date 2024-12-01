6ab5df1123c0e3a7769f935347455d4fe305774c
yuankuang <kuang.yua@northeastern.edu>
2024-12-01 15:36:19 -0800

    added findByCategoryName method in the interface, JPA will handle this query automatically, follow the name convention to match the field name in the db

--- src/main/java/com/ecommerce/project/repository/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.ecommerce.project.repository;
 
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ecommerce.project.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(@NotBlank String categoryName);
 }
