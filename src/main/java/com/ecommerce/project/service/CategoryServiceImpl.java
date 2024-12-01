be58043a80315b789504f9e9e9cc91394425241a
yuankuang <kuang.yua@northeastern.edu>
2024-12-01 15:39:31 -0800

    no need to handle the increment of categoryId manually since the @GeneratedValue is in use; added exception handling logic in CategoryServiceImpl;

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIExceptions;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +19,37 @@ public class CategoryServiceImpl implements CategoryService{
     private CategoryRepository categoryRepository;
 //    private List<Category> categories = new ArrayList<>(); // no NullPointerException when accessing categories
     private Long nextId = 1L; //use this to keep track of id
+
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> all = categoryRepository.findAll();
+        if(all.isEmpty()){
+            throw new APIExceptions("no category found.");
+        }
+        return all;
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
+//        category.setCategoryId(nextId++);
         // assign id automatically
+        //add validation, jpa handles the query automatically by the findByCategoryName automatically
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        //if it exists in the db
+        if(savedCategory != null){
+            throw new APIExceptions("category with name " + category.getCategoryName()+"already exists.");
+        }
         categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        //add a check to prevent category not found error
-       Category category = categories.stream()
-               .filter(c -> c.getCategoryId().equals(categoryId))
-               .findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "resource not found"));
+//        List<Category> categories = categoryRepository.findAll();
+//        //add a check to prevent category not found error
+//       Category category = categories.stream()
+//               .filter(c -> c.getCategoryId().equals(categoryId))
+//               .findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "resource not found"));
+        Category category = categoryRepository.findById(categoryId).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "not found"));
        categoryRepository.delete(category);
        return "category with id " + categoryId + " deleted";
 
@@ -51,6 +64,7 @@ public class CategoryServiceImpl implements CategoryService{
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
             Category savedCategory = categoryRepository.save(existingCategory);
+            //System.out.println("updated category: " + savedCategory); //updated category: Category{categoryId=1, categoryName='xx'}
             return savedCategory;
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found");
